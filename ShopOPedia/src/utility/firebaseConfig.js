a576eddab2e68767e3f7f27edf8b5579d7cc1dfe
Bhrugen Patel <bhrugen1990@gmail.com>
2025-03-10 13:53:21 -0500

    Section 19 - Create App in Firebase

---
+++ ShopOPedia/src/utility/firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAqItBnejr-4q29MVYbmgkTXOPhFWZfYhg',
+  authDomain: 'shopopedia-2431b.firebaseapp.com',
+  projectId: 'shopopedia-2431b',
+  storageBucket: 'shopopedia-2431b.firebasestorage.app',
+  messagingSenderId: '889686619259',
+  appId: '1:889686619259:web:29e5621c0f3c9c451bdede',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
+export { app as firebaseApp }
