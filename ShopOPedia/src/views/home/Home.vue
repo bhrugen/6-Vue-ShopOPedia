0c8e8564856179346c87b50a6c5f8b7aa2eecd2f
Bhrugen Patel <bhrugen1990@gmail.com>
2025-03-10 23:04:28 -0500

    Section 21 - Get Product List for Home Page

--- ShopOPedia/src/views/home/Home.vue
+++ ShopOPedia/src/views/home/Home.vue
@@ -1,1 +1,25 @@
 <template>Homes</template>
+
+<script setup>
+import { onMounted, ref } from 'vue'
+import productService from '@/services/productService'
+
+const products = ref([])
+const loading = ref(false)
+
+onMounted(() => {
+  fetchProducts()
+})
+
+const fetchProducts = async () => {
+  try {
+    loading.value = true
+    products.value = await productService.getProducts()
+    console.log(products.value)
+  } catch (err) {
+    console.log(err)
+  } finally {
+    loading.value = false
+  }
+}
+</script>
