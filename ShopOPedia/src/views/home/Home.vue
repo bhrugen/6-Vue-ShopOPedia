1c8cee17d96f298c1f7df1c699b80d9322d920ac
Bhrugen Patel <bhrugen1990@gmail.com>
2025-03-11 10:56:35 -0500

    Section 21 - Sort in Action

--- ShopOPedia/src/views/home/Home.vue
+++ ShopOPedia/src/views/home/Home.vue
@@ -64,7 +64,10 @@
             </button>
             <ul class="dropdown-menu dropdown-menu-end shadow-sm">
               <li v-for="(sort, index) in SORT_OPTIONS" :key="index">
-                <button class="dropdown-item py-2 d-flex align-items-center gap-2">
+                <button
+                  class="dropdown-item py-2 d-flex align-items-center gap-2"
+                  @click="selectedSortOption = sort"
+                >
                   <i class="bi"></i>
                   <span class="text-capitalize"> {{ sort }} </span>
                 </button>
@@ -136,6 +139,19 @@ const filteredProductList = computed(() => {
     tempArray = tempArray.filter((item) => {
       return item.name.toUpperCase().includes(searchValue.value.toUpperCase())
     })
+  }
+
+  if (selectedSortOption.value === SORT_NAME_A_Z) {
+    tempArray.sort((a, b) => a.name.localeCompare(b.name))
+  }
+  if (selectedSortOption.value === SORT_NAME_Z_A) {
+    tempArray.sort((a, b) => b.name.localeCompare(a.name))
+  }
+  if (selectedSortOption.value === SORT_PRICE_HIGH_LOW) {
+    tempArray.sort((a, b) => b.price - a.price)
+  }
+  if (selectedSortOption.value === SORT_PRICE_LOW_HIGH) {
+    tempArray.sort((a, b) => a.price - b.price)
   }
 
   return tempArray
