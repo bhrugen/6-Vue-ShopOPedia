1968da3d4aa2cc563de504009fd0cf75bd72994f
Bhrugen Patel <bhrugen1990@gmail.com>
2025-03-11 20:19:29 -0500

    Section 24 - Persist Pinia State

--- ShopOPedia/src/main.js
+++ ShopOPedia/src/main.js
@@ -7,14 +7,26 @@ import router from './router/routes'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import { useThemeStore } from './stores/themeStore'
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
 
-let app
+const app = createApp(App)
+app.use(pinia)
+app.use(router)
+
+const themeStore = useThemeStore()
+if (themeStore.theme) {
+  const bodyElement = document.body
+  bodyElement.setAttribute('data-bs-theme', themeStore.theme)
+}
+
+let isMounted = false
 
 onAuthStateChanged(auth, async (user) => {
-  if (!app) {
-    app = createApp(App)
-    app.use(createPinia())
-    app.use(router)
+  if (!isMounted) {
     app.mount('#app')
+    isMounted = true
   }
 })
--- ShopOPedia/src/stores/themeStore.js
+++ ShopOPedia/src/stores/themeStore.js
@@ -1,16 +1,22 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
-export const useThemeStore = defineStore('themeStore', () => {
-  const theme = ref('dark')
+export const useThemeStore = defineStore(
+  'themeStore',
+  () => {
+    const theme = ref('dark')
 
-  const setTheme = (newTheme) => {
-    theme.value = newTheme
-    const bodyElement = document.body
-    bodyElement.setAttribute('data-bs-theme', newTheme)
-  }
+    const setTheme = (newTheme) => {
+      theme.value = newTheme
+      const bodyElement = document.body
+      bodyElement.setAttribute('data-bs-theme', newTheme)
+    }
 
-  return {
-    theme,
-    setTheme,
-  }
-})
+    return {
+      theme,
+      setTheme,
+    }
+  },
+  {
+    persist: true,
+  },
+)
