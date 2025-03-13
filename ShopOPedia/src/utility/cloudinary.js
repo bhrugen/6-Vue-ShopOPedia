b536d6e3ef234bd91a79cc47e00716f7c4479aa7
Bhrugen Patel <bhrugen1990@gmail.com>
2025-03-12 20:25:15 -0500

    Section 25 - Cloudinary Upload

--- ShopOPedia/src/utility/cloudinary.js
+++ ShopOPedia/src/utility/cloudinary.js
@@ -4,7 +4,7 @@ export const uploadToCloudinary = async (file) => {
   formData.append('upload_preset', 'Free Public Upload')
 
   try {
-    const response = await fetch('https://api.couldinary.com/v1_1/debxhug5q/image/upload', {
+    const response = await fetch('https://api.cloudinary.com/v1_1/debxhug5q/image/upload', {
       method: 'POST',
       body: formData,
     })
